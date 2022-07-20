f2453bcc1790b43bf0c6ddb1c1ca30651d225338
flyCat1350 <1113585882@qq.com>
2022-07-20 11:39:59 +0800

    fix bug

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" @click="triggerEmit(1)"/>
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    // this.$on('trigger-emit', data => {
+    //   alert(data)
+    // })
+  },
+  methods: {
+    triggerEmit(val) {
+      this.$emit('trigger-emit', val)
+    }
+  }
+}
+</script>
---
+++ HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="triggerEmit(1)">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    triggerEmit(val) {
+      // this.$emit('trigger-emit', val)
+      this.count++
+    }
+  },
+}
+</script>
